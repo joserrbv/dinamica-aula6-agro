ac744b4d37826029c7ed62c29e2dfbc3332afb0c
joserrbv <josequeiroz40@gmail.com>
2023-01-22 13:03:19 -0400

    dinamica aula 6 com js

---
+++ boot.js
@@ -0,0 +1,19 @@
+// Get the modal
+var modal = document.getElementById("modalin");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modall");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// quando cliacar no <span> (x), fecha o modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
